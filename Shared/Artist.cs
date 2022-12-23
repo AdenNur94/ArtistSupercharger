3aa3ef1b73d451c5d8a33c9e07c7227b3cda0300
User <User@USER-PC>
2022-12-23 01:12:48 +0100

    Add project files.

---
+++ Shared/Artist.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtistSupercharger.Shared
+{
+    public class Artist
+    {
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public int Popularity { get; set; }
+        public int ArtistID { get; set; }
+        public int Followers { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
